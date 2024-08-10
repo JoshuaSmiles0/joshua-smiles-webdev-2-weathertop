7673df294ff7dca1b95f3241fd79f15bc83f0af9
Glitch (joshua-smiles-weathertop) <none>
2024-08-10 16:23:23 +0000

    🌉🛥️ Updated with Glitch

--- routes.js
+++ routes.js
@@ -7,7 +7,6 @@ import {accountsController} from "./controllers/accounts-controller.js";
 
 export const router = express.Router();
 
-// router.get("/", dashboardController.index);
 router.get("/dashboard", dashboardController.index);
 router.get("/about", aboutController.index);
 router.post("/dashboard/addstation",dashboardController.addStation);
